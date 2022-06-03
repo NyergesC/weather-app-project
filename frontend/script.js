b0fd1fff1d405fbaace8e5337ddfbc58af06859d
NyergesC <cintia.nyerges@gmail.com>
2022-06-03 17:09:15 +0200

    fetch object added

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+const fetchOpt = {                          
+    key: 'key=a21bbc1401c94828958202032220803', 
+    url: 'http://api.weatherapi.com/v1',        
+    methodes: {
+      search: '/search.json',
+      forecast: '/forecast.json',
+      current: '/current.json'
+    },
+    query: 'q='     
+  };
+  
+  window.addEventListener('load', onHTMLParsingFinished);
+
+  
